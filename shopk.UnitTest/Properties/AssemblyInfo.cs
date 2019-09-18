977611e6892131e30a28569f1433fa98daf31373
nguyenhuukhanhphat@gmail.com <nguyenhuukhanhphat@gmail.com>
2019-09-18 22:33:00 +0700

    Có 3 trạng thái commit: 0-Stage All là chưa. 1-Là commit bước đang làm

---
+++ shopk.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("shopk.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("shopk.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc7dbb79-2de1-42fc-a2ea-d27fe74d98b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
